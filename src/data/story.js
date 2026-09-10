74a72d75a19e83bf71e4a98392aff7a63fd00a64
Yuvan12128 <yuvan12128@gmail.com>
2026-09-10 12:52:35 +0530

    Update 10th Card

--- src/data/story.js
+++ src/data/story.js
@@ -207,6 +207,11 @@ export const storyData = {
         num: "09",
         title: "A Comfortable Ride 🚙",
         text: "College function-kku en kooda car-la vandha andha journey, nee romba comfortable-ah irundhadhu enakku oru memorable moment. Aana andha naal unakku food vaangi thara mudiyala… adhukku really sorry. Appove unakku edhavadhu vaangi thandhirukkanum-nu ippo ninaikkiren. 🥺"
+      },
+      {
+        num:"10",
+        title:"That Song 🎶",
+        text:"Oru naal nee ‘Honny Honny’ song paadi enakku send pannina… honestly, romba nalla irundhuchu. Nee paadina way romba nice-ah irundhuchu 🤍..."
       }
     ]
   },
